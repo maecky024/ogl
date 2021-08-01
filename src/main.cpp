e66973035c3d6056fb7107dcd19273811521a333
Markus Winkler <maecky@gmail.com>
2021-08-01 16:58:40 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+int main(int, char**) {
+
+    std::cout << "Hello, world!\n";
+
+    if (!glfwInit())
+    {
+        // Initialization failed
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+    if (!window)
+    {
+        // Window or OpenGL context creation failed
+    }
+    glfwSetKeyCallback(window, key_callback);
+
+    glfwMakeContextCurrent(window);
+    glClearColor(0.0f,0.0f,0.0f,0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // Keep running
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
